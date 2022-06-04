f7cf877938773bfeae5caf6710b20f1bb9142c77
Carlos Rivas <Cjrivas00@gmail.com>
2022-06-03 20:17:57 -0400

    changed the fonts added the router in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,26 @@
 import React, { useState } from "react";
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Intro from "./intro";
+// import { ChakraProvider } from '@chakra-ui/react';
+// import Nav from "./components/Nav";
+// import Header from "./components/Header";
 
-export const App = () => {
-   return (
-      <Parallax pages={2} style={{ top: "0", left: "0" }}>
-         <ParallaxLayer
-            offset={0}
-            speed={2.5}
-            style={{
-               display: "flex",
-               justifyContent: "center",
-               alignItems: "center",
-            }}
-         >
-         <p>Scroll down</p>
-         </ParallaxLayer>
-         <ParallaxLayer
-            offset={1}
-            speed={2}
-            style={{ backgroundColor: "#ff6d6d" }}
-             />
 
-         <ParallaxLayer
-            offset={1}
-            speed={0.5}
-            style={{
-               display: "flex",
-               justifyContent: "center",
-               alignItems: "center",
-               color: "white",
-            }}>
-            <p>Scroll up </p>
-         </ParallaxLayer>
-      </Parallax>
-   );
-}
+const App = () => {
+  const [pages] = useState([
 
+
+   
+  ])
+  return (
+
+  <Router>
+    
+  <Intro/>z
+
+  </Router>
+   
+  );
+};
 
 export default App;
---
+++ src/intro.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import {Parallax, ParallaxLayer} from '@react-spring/parallax';
+import dungeon from './images/dungeon.gif' 
+import { Button, ButtonGroup, color } from '@chakra-ui/react'
+
+export const intro = () => {
+  return (
+    <Parallax pages={1} style={{ top: "0", left: "0" }}>
+        
+   <ParallaxLayer
+      offset={0}
+     
+      style={{
+        backgroundImage:`url(${dungeon})`,
+
+        height:'auto',
+        backgroundSize:`cover`,
+         display: "flex",
+         justifyContent: "center",
+         alignItems: "center",
+      }}
+   >
+      </ParallaxLayer>
+
+
+      <ParallaxLayer> 
+     <h1 style={{color:'	#deb887',
+                display:'flex',
+                margin:'30px',
+                marginBottom:'20rem',
+                  fontFamily:'Cursive'
+    }}>Dungeon Disco</h1>
+     <h2 style={{
+                fontFamily:'fantasy',
+                fontSize:'2.5rem',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                margin:'20px',
+                marginBottom:'10rem',
+                color:'	#dc143c',
+     }}>Enter at your own risk! </h2>
+     <div style={{
+        display:'flex',
+        alignItems: "center",
+justifyContent:'flex-end'
+     }}> 
+<button style={{marginRight:'8rem',
+background: 'transparent',
+padding:'20px 45px',
+color:'white',
+outlineStyle:'double',
+outlineColor:'brown',
+fontSize:'1.2rem',
+fontFamily:'fantasy'
+}}>
+   Enter
+</button>
+   </div>
+   </ParallaxLayer>   
+
+</Parallax>
+  )
+}
+
+export default intro;
