687a38ac9632fa512e1830d1a6dc495114b01cb8
Sam Clark <samclark2399@gmail.com>
2022-06-06 22:12:21 -0400

    put all the files into correct structure. Backend base is set up. Can create users and query them.

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,57 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                .populate('characters')
+          
+              return userData;
+            }
+          
+            throw new AuthenticationError('Not logged in');
+          },
+          // get all users
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            // .populate('characters')
+        },
+        // get a user by username
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            // .populate('characters')
+        },
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+          
+            return { token, user };
+          },
+        login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+        
+        if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+        
+        const correctPw = await user.isCorrectPassword(password);
+        
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+        
+        const token = signToken(user);
+        return { token, user };
+        },
+    }
+}
+
+module.exports = resolvers;
