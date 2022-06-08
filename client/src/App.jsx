4ec3b706f8a386520abadd3821b434ad38240094
Sam Clark <samclark2399@gmail.com>
2022-06-08 19:01:53 -0400

    added routes to app.js in client/src

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,74 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Intro from "./intro";
-import Login from "./components/Login"; 
-
 import './App.css'; 
+import Intro from "./intro";
 
-// import Nav from "./components/Nav";
-// import Header from "./components/Header";
+import Login from "./pages/LogIn";
+import CharacterChar from "./pages/CharacterChar";
+import MyCharacters from "./pages/MyCharacters";
+import SignUp from "./pages/SignUp";
 
+import Navbar from "./components/Navbar";
+
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
-  const [pages] = useState([
-
-
-   
-  ])
   return (
-
-  <Router>
-    
-  {/* <Intro/> */}
-  <Login/>
-
-  </Router>
-   
+    <ApolloProvider client={client}>
+    <Router>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={<Intro />}
+            />
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+            <Route
+              path="/signup"
+              element={<SignUp />}
+            />
+            <Route
+              path="/profile/"
+              element={<MyCharacters />}
+            />
+            <Route
+              path="/profile/"
+              element={<CharacterChar />}
+            />
+            {/* <Route
+              path="*"
+              element={<NoMatch />}
+            /> */}
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  </ApolloProvider>
   ); 
 };
 
